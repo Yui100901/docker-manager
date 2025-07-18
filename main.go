deee5855c7273be08581b246cda54040f62ba4d3
Yui100901 <973800960@qq.com>
2025-07-18 09:51:09 +0800

    添加命令

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
+import (
+	"docker-manager/pull"
+	"docker-manager/reverse"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
 //
 // @Author yfy2001
 // @Date 2025/7/18 09 43
 //
 
 func main() {
+	rootCmd := &cobra.Command{
+		Use:   "dm <command>",
+		Short: "Docker小工具，可用于管理容器.\nAuthor:南城琉璃霞",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := cmd.Help()
+			if err != nil {
+				return
+			}
+		},
+	}
 
+	rootCmd.AddCommand(newBuildCommand())
+	rootCmd.AddCommand(newCleanCommand())
+	rootCmd.AddCommand(newImportCommand())
+	rootCmd.AddCommand(newExportCommand())
+	rootCmd.AddCommand(reverse.NewReverseCommand())
+	rootCmd.AddCommand(pull.NewPullCommand())
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
